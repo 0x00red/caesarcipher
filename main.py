fedaab2df0bca6d366673b5149958cc2b85100e1
0x00red <43559748+0x00red@users.noreply.github.com>
2018-09-27 08:35:21 -0400

    added support for punctuation, fixed a crash
    punctuation is now passed through the cipher, more secure ways of passing punctuation may be developed later
    fixed a crash on input where len(input) == 0

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
 import sys
 ls = "abcdefghijklmnopqrstuvwxyz" #string of characters
+punct = "\"'.,!?;:" ##punctuation is skipped
 dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25} #dictionary of letter to index pairing
 input = raw_input ##hack-ish fix since shit wasn't working
 
 ##functions and shit
 
-def encrypt(instring, shift, ls, dict): #takes input text, shift, and a list and dictionary(for language support)
+def encrypt(instring, shift, ls, dict, punct): #takes input text, shift, and a list and dictionary(for language support)
 	instring = instring.split()
 	out = ""
 	for word in instring:
 		for letter in word:
+			if letter in punct:
+				out = out + letter
+				continue
 			out = out + ls[(dict[letter] + shift) % len(ls)]
 		out = out + " "
 	return out[:-1]
 
 
-def decrypt(cipher, shift, ls, dict): #takes cipher, shift, list and dict(lang support) to decipher
+def decrypt(cipher, shift, ls, dict, punct): #takes cipher, shift, list and dict(lang support) to decipher
 	cipher = cipher.split()
 	out = ""
 	for word in cipher:
 		for letter in word:
+			if letter in punct:
+				out = out + letter
+				continue
 			out = out + ls[(dict[letter] - shift) % len(ls)]
 		out = out + " "
 	return out[:-1]
@@ -31,6 +38,8 @@ def decrypt(cipher, shift, ls, dict): #takes cipher, shift, list and dict(lang s
 while(1): ##makes sure the user selects a valid option
 	print("[E]ncrypt or [D]ecrypt? ")
 	eord = input()
+	if len(eord) == 0:
+		continue
 	eord = eord.lower()
 	if eord[0] == "e":
 		break
@@ -54,11 +63,11 @@ except:
 ##does encryption or decryption and posts output
 if eord[0] == "e":
 	try:
-		print(encrypt(instring, shift, ls, dict))
+		print(encrypt(instring, shift, ls, dict, punct))
 	except:
 		print("Only alphabetical characters and whitespace allowed.")
 elif eord[0] == "d":
 	try:
-		print(decrypt(instring, shift, ls, dict))
+		print(decrypt(instring, shift, ls, dict, punct))
 	except:
 		print("Only alphabetical characters and whitespace allowed.")
